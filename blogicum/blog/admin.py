84a11f6fcd434062a07a6b17be2d0b5743866bf9
myffii <132840123+myffii@users.noreply.github.com>
2025-12-23 01:03:18 +0300

    Рекомендации

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,7 +2,37 @@ from django.contrib import admin  # type: ignore[import-untyped]
 
 from .models import Category, Location, Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'pub_date',
+        'author',
+        'category',
+        'location',
+        'is_published',
+    )
+    list_editable = (
+        'is_published',
+        'category',
+    )
+    list_filter = ('is_published', 'category', 'pub_date')
+    search_fields = ('title', 'text')
+    date_hierarchy = 'pub_date'
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'is_published',
+    )
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'is_published',
+    )
